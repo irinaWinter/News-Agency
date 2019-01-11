bc0a326e7219992e5ad349ec06320e4bc965394c
irinaWinter <lobanova.irina.96@mail.ru>
2019-01-11 16:30:15 +0300

    Работа над мигающим двоеточием

--- script/show_date_and_time.js
+++ script/show_date_and_time.js
@@ -8,18 +8,24 @@ $(function() {
 	var monthNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 	if (monthNumber[month] < 10) monthNumber[month] = "0" + monthNumber[month];
 	var year = date.getFullYear();
-	var timeAndDate = hours + "<span class='header__span_style_blink'>:</span>" + minutes + " " + day + "." + monthNumber[month] + "." + year;
-	// function blink() {
-	// 	if ($('.header__span_style_blink') == ":") {
-	// 		$('.header__span_style_blink') == "dkfv";
-	// 	} else {
-	// 		$('.header__span_style_blink') == ":";
-	// 	}
-	// }
+	var colon = ":";
+	var timeAndDate = hours + "<span class='header__span_style_blink'>" + blink() + "</span>" +
+					  minutes + " " + day + "." + monthNumber[month] + "." + year;
 	function showDateAndTime () {
 		var dateAndTimeBlock = document.getElementsByClassName("header__text_dateTime");
 		dateAndTimeBlock[0].innerHTML = timeAndDate;
-	}
+	}	
+	// Доработать мигание двоеточия			  
+	function blink() { 
+		if (colon === ":") {		
+			return colon = " ";
+		} else {
+			return colon = ":";
+		}
+		
+	};	
+	console.log(blink());
+	
 	showDateAndTime();
-	// blink();
+	setTimeout(showDateAndTime, 1000);
 });	
