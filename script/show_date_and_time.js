0a7dee49c6760427f34454442da8699ca4336fa9
irinaWinter <lobanova.irina.96@mail.ru>
2019-01-11 12:28:09 +0300

    Корректное отображение даты и времени (добавить мигание двоеточию)

--- script/show_date_and_time.js
+++ script/show_date_and_time.js
@@ -1,14 +1,25 @@
-var date = new Date();
-var hours = date.getHours();
-var minutes = date.getMinutes();
-var day = date.getDay();
-var month = date.getMonth();
-var year = date.getFullYear();
-var timeAndDate = hours + " " + minutes + " " + day + " " + month + " " + year;
-
-function showDateAndTime () {
-	var dateAndTimeBlock = document.getElementsByClassName("header__text_dateTime");
-	dateAndTimeBlock[0].innerHTML = timeAndDate;
-}
-showDateAndTime();
-// alert(timeAndDate);
+$(function() {	
+	var date = new Date();
+	var hours = date.getHours();
+	var minutes = date.getMinutes();
+	if (minutes < 10) minutes = "0" + minutes;
+	var day = date.getDate();
+	var month = date.getMonth();
+	var monthNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+	if (monthNumber[month] < 10) monthNumber[month] = "0" + monthNumber[month];
+	var year = date.getFullYear();
+	var timeAndDate = hours + "<span class='header__span_style_blink'>:</span>" + minutes + " " + day + "." + monthNumber[month] + "." + year;
+	// function blink() {
+	// 	if ($('.header__span_style_blink') == ":") {
+	// 		$('.header__span_style_blink') == "dkfv";
+	// 	} else {
+	// 		$('.header__span_style_blink') == ":";
+	// 	}
+	// }
+	function showDateAndTime () {
+		var dateAndTimeBlock = document.getElementsByClassName("header__text_dateTime");
+		dateAndTimeBlock[0].innerHTML = timeAndDate;
+	}
+	showDateAndTime();
+	// blink();
+});	
