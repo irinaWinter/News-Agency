46e5912b621f11d44e4a87813a57ac53118abed1
irinaWinter <lobanova.irina.96@mail.ru>
2019-02-01 16:40:10 +0300

    Корректное отображение дня

--- script/show_date_and_time.js
+++ script/show_date_and_time.js
@@ -16,6 +16,7 @@ $(function() {
 		var minutes = date.getMinutes();
 		if (minutes < 10) minutes = "0" + minutes;
 		var day = date.getDate();
+		if (day < 10) day = "0" + day;
 		var month = date.getMonth();
 		var monthNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 		if (monthNumber[month] < 10) monthNumber[month] = "0" + monthNumber[month];
